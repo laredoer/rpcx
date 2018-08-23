478125ff88c5070e2421493219d0522ce9844387
wule61 <wule61@live.com>
2018-08-23 21:36:12 +0800

    加入gin,可以通过http访问

--- cp2/client/client.go
+++ cp2/client/client.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"log"
-	"time"
+	"thresher/cp2/handler"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/smallnest/rpcx/client"
-
-	"thresher/cp2/handler"
 )
 
 var (
@@ -16,6 +15,7 @@ var (
 )
 
 func main() {
+
 	flag.Parse()
 
 	d := client.NewPeer2PeerDiscovery("tcp@"+*addr, "")
@@ -26,16 +26,20 @@ func main() {
 		A: 10,
 		B: 20,
 	}
+	r := gin.Default()
 
-	for {
+	r.GET("/ping", func(c *gin.Context) {
+
 		reply := &handler.Reply{}
-		err := xclient.Call(context.Background(), "Mul", args, reply)
+		err := xclient.Call(c, "Mul", args, reply)
 		if err != nil {
 			log.Fatalf("failed to call: %v", err)
 		}
-
 		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
-		time.Sleep(1e9)
-	}
+		c.JSON(200, gin.H{
+			"message": reply,
+		})
+	})
+	r.Run() // 在 0.0.0.0:8080 上监听并服务
 
 }
