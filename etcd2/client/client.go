5c45baada212bd8fc52e343d458779857acaea1e
wule61 <wule61@live.com>
2018-08-29 23:35:48 +0800

    -

--- etcd2/client/client.go
+++ etcd2/client/client.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"flag"
-	"github.com/smallnest/rpcx/client"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rpcx-ecosystem/rpcx-examples3"
-	"log"
+	"github.com/smallnest/rpcx/client"
 )
 
 var (
 	etcdAddr = flag.String("etcdAddr", "localhost:2379", "etcd address")
 	basePath = flag.String("base", "/rpcx", "prefix path")
-	xclient client.XClient
+	xclient  client.XClient
 )
 
 func init() {
@@ -20,27 +21,24 @@ func init() {
 	d := client.NewEtcdDiscovery(*basePath, "Arith", []string{*etcdAddr}, nil)
 	xclient = client.NewXClient("Arith", client.Failover, client.RoundRobin, d, client.DefaultOption)
 
-
 }
-
-
 
 func main() {
 	defer xclient.Close()
 	g := gin.Default()
-	g.GET("/ping",Get)
+	g.GET("/ping", Get)
 	g.Run(":8888")
 }
 
-func Get(g *gin.Context)  {
+func Get(g *gin.Context) {
 	args := &example.Args{
 		A: 10,
 		B: 20,
 	}
-		reply := &example.Reply{}
-		err := xclient.Call(g, "Mul", args, reply)
-		if err != nil {
-			log.Printf("failed to call: %v\n", err)
-		}
-		log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+	reply := &example.Reply{}
+	err := xclient.Call(g, "Mul", args, reply)
+	if err != nil {
+		log.Printf("failed to call: %v\n", err)
+	}
+	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
 }
